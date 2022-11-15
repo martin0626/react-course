33e1f34138eaf6bfdd185ff28dc50a3024ee8daf
martin0626 <scbsteel12@gmail.com>
2022-11-15 14:11:12 +0200

    Added User Input Functionality

--- src/Components/NewExpense/NewExpenseForm.js
+++ src/Components/NewExpense/NewExpenseForm.js
@@ -1,19 +1,66 @@
+import { useState } from "react";
 import "./NewExpenseForm.css";
 
-function ExpenseForm() {
+function ExpenseForm(props) {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  let titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  let amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  let dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenseData(expenseData);
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+        ></input>
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" min="0.01" step="0.01"></input>
+        <input
+          type="number"
+          min="0.01"
+          step="0.01"
+          value={enteredAmount}
+          onChange={amountChangeHandler}
+        ></input>
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" min="2019-01-01"></input>
+        <input
+          type="date"
+          min="2019-01-01"
+          value={enteredDate}
+          onChange={dateChangeHandler}
+        ></input>
       </div>
       <div className="new-expense__actions">
         <button>Add Expense</button>
